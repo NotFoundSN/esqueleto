0b661dae3114e166d58f41d90dfb16f4bb64f371
Julio Maldonado <38104075@DRafam>
2022-09-23 11:33:55 -0300

    ADD estructura base del proyecto

---
+++ app.js
@@ -0,0 +1,29 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const mainRouter = require('./routes/main');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', mainRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+});
+
+module.exports = app;
---
+++ controllers/mainController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    principal : (req,res) => {
+        res.send("hola");
+    }
+}
